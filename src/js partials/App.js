5a6c32693ce38cbd77adef2aa61069d28bb2c3e1
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-10-21 19:02:05 +0300

    refactor: hide unneeded lines of code for now

--- src/js partials/App.js
+++ src/js partials/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../css partials/App.css";
 
 export default function App(newCity) {
--- src/js partials/InputForm.js
+++ src/js partials/InputForm.js
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 import "../css partials/InputForm.css";
 
 export default function InputForm() {
   const [city, setCity] = useState("");
-  const [weather, setWeather] = useState({});
-  const [loaded, setLoaded] = useState(false);
+  // const [weather, setWeather] = useState({});
+  // const [loaded, setLoaded] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
     const API_KEY = "8f5def28851f5cf815cb404385490799";
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-    axios.get(API_URL).then(showWeather);
+    // axios.get(API_URL).then(showWeather);
   }
 
   function updateInput(e) {
     setCity(e.target.value);
   }
 
-  function showWeather({ data }) {
-    setLoaded(true);
-    setWeather({
-      temp: data.main.temp,
-      desc: data.weather[0].description,
-      humidity: data.main.humidity,
-      wind: data.wind.speed,
-      icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-    });
-  }
+  // function showWeather({ data }) {
+  //   setLoaded(true);
+  //   setWeather({
+  //     temp: data.main.temp,
+  //     desc: data.weather[0].description,
+  //     humidity: data.main.humidity,
+  //     wind: data.wind.speed,
+  //     icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+  //   });
+  // }
 
   return (
     <div className="input-form InputForm">
